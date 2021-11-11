c97aa05cbf7dbd1d912a7f963b34745371ee3f34
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-11-10 21:53:31 -0500

    achieved mvp, errors with yup

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // import Form
@@ -10,35 +10,32 @@ import * as yup from 'yup';
 
 // import axios and User
 import axios from 'axios';
-import User from './components/User';
+// import User from './components/User';
 
 // initial states
-const initialFormValues = {  username: '', password: '', email: '', tos: false }
-const initialFormErrors = { username: '', password: '', email: '', tos: '' }
-const initialUsers = [];
+const initialFormValues = { username: '', password: '', email: '', tos: false };
+const initialFormErrors = { username: '', password: '', email: '', tos: '' };
 
 function App() {
 
   // states
   const [formValues, setFormValues] = useState(initialFormValues);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [users, setUsers] = useState(initialUsers);
+  const [users, setUsers] = useState([]);
 
   const handleSubmit = () => {
     axios.post('https://reqres.in/api/users', formValues)
       .then(res => {
-        // console.log(res);
-        setUsers(res.data);
+        setUsers([res.data, ...users]);
       })
       .catch(err => console.error(err))
       .finally(() => setFormValues(initialFormValues));
   }
 
   const validate = (name, value) => {
-    yup.reach(schema, name)
-      .validate(value)
+    yup.reach(schema, name).validate(value)
       .then(() => setFormErrors({ ...formErrors, [name]: '' }))
-      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
   }
 
   const handleChange = (name, value) => {
@@ -46,16 +43,16 @@ function App() {
     setFormValues({...formValues, [name]: value})
   }
 
-  useEffect(() => {
-    axios.get('https://reqres.in/api/users')
-      .then(res => {
-        // console.log(res.data.data);
-        setUsers(res.data.data);
-      })
-      .catch(err => {
-        console.error(err);
-      })
-  }, [])
+  // useEffect(() => {
+  //   axios.get('https://reqres.in/api/users')
+  //     .then(res => {
+  //       // console.log(res.data.data);
+  //       setUsers(res.data);
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     })
+  // }, [])
 
   return (
     <div className="App">
@@ -65,14 +62,12 @@ function App() {
         errors={formErrors}
         submit={handleSubmit}
       />
-
-      { users.map(user => {
-        return (
-          <User
-            
-          />
-        )
-      }) }
+      {users.map(user => (
+        <div key={user.id}>
+          <p>{user.createdAt}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
